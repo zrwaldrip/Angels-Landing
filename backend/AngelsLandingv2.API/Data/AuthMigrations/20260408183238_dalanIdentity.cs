8d24ff802501fb8e31844d0515adc705b220316c
Dalan Cluff <dalan.cluff@gmail.com>
2026-04-08 13:32:33 -0600

    CRUD Functionalities added to medical/education records

---
+++ backend/AngelsLandingv2.API/Data/AuthMigrations/20260408183238_dalanIdentity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AngelsLandingv2.API.Data.AuthMigrations
+{
+    /// <inheritdoc />
+    public partial class dalanIdentity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
